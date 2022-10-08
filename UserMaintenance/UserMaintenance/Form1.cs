8cd0f006b4f4fe01b121508e661a4a97202f4480
gmolnar2022 <geza.molnar2@uni-corvinus.hu>
2022-10-08 14:52:38 +0200

    feladat 6

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -15,6 +15,9 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            lblFirstName.Text = Resource1.FirstName;
+            lblLastName.Text = Resource1.LastName;
+            btnAdd.Text = Resource1.Add;
         }
 
     }
