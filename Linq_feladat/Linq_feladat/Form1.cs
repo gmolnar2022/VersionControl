3fdc928c9c9e020b1ebd145414f8796fa2dc9e6f
gmolnar2022 <geza.molnar2@uni-corvinus.hu>
2022-11-12 10:18:34 +0100

    feladat3

--- Linq_feladat/Linq_feladat/Form1.cs
+++ Linq_feladat/Linq_feladat/Form1.cs
@@ -14,6 +14,7 @@ namespace Linq_feladat
     public partial class Form1 : Form
     {
         List<Country> countries = new List<Country>();
+        List<Ramen> ramens = new List<Ramen>();
 
         public Form1()
         {
@@ -30,6 +31,12 @@ namespace Linq_feladat
                 string[] sor = sr.ReadLine().Split(';');
                 string orszag = sor[2];
                 //    var ered = countries.Where(i => i.Name.Equals(orszag)).FirstOrDefault(); //LINQ
+                AddCountry(orszag);
+            }
+            sr.Close();
+
+            void AddCountry(string orszag)
+            {
                 var ered = (from c in countries where c.Name.Equals(orszag) select c).FirstOrDefault();
                 if (ered == null)  //nincs ilyen ország a listában
                 {
@@ -41,8 +48,6 @@ namespace Linq_feladat
                     countries.Add(ered);
                 }
             }
-            sr.Close();
-
         }
 
     }
