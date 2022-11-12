0954bbca544f72bca83ab48ac598444bc211fa32
gmolnar2022 <geza.molnar2@uni-corvinus.hu>
2022-11-12 09:17:17 +0100

    feladat1

---
+++ Linq_feladat/Linq_feladat/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_feladat
+{
+    class Country
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+
+
+}
