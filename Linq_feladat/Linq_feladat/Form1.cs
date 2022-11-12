bfff2cf49d60151501647b54a1568a375a30c38d
gmolnar2022 <geza.molnar2@uni-corvinus.hu>
2022-11-12 10:12:02 +0100

    feladat2

--- Linq_feladat/Linq_feladat/Form1.cs
+++ Linq_feladat/Linq_feladat/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,36 @@ namespace Linq_feladat
 {
     public partial class Form1 : Form
     {
+        List<Country> countries = new List<Country>();
+
         public Form1()
         {
             InitializeComponent();
+            LoadData("ramen.csv");
+        }
+
+        void LoadData(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            sr.ReadLine(); //átugrik az első soron
+            while (!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(';');
+                string orszag = sor[2];
+                //    var ered = countries.Where(i => i.Name.Equals(orszag)).FirstOrDefault(); //LINQ
+                var ered = (from c in countries where c.Name.Equals(orszag) select c).FirstOrDefault();
+                if (ered == null)  //nincs ilyen ország a listában
+                {
+                    ered = new Country
+                    {
+                        ID = countries.Count,
+                        Name = orszag
+                    };
+                    countries.Add(ered);
+                }
+            }
+            sr.Close();
+
         }
 
     }
