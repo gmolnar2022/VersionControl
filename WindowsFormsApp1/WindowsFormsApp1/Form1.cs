2c62fbf54bb9f90426cffa1c2e41c0d0374af6d7
gmolnar123 <geza.molnar2@uni-corvinus.hu>
2022-11-19 17:30:52 +0100

    feladat6

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -17,10 +17,42 @@ namespace WindowsFormsApp1
         List<Flat> flats;
         RealEstateEntities re = new RealEstateEntities();
 
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
 
         void LoadData()
         {
             flats = re.Flat.ToList();
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add();
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Source + '\n' + ex.Message);
+                xlWB.Close(false);
+                xlApp.Quit();
+                xlApp = null;
+                xlWB = null;
+            }
+
+        }
+
+        private void CreateTable()
+        {
+            
         }
 
         public Form1()
