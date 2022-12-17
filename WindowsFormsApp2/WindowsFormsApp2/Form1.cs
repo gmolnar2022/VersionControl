765ff447626cbff01f12b32afb313a1ae824d1af
gmolnar2022 <geza.molnar2@uni-corvinus.hu>
2022-12-17 13:59:57 +0100

    feladat07

--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -35,5 +35,20 @@ namespace WindowsFormsApp2
             dataGridView2.DataSource = portfolio;
 
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in portfolio)
+            {
+                var last = (from x in ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
