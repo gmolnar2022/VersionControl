d662ae16ace730278056d2d8702c3799776c3207
gmolnar2022 <geza.molnar2@uni-corvinus.hu>
2022-12-17 13:54:56 +0100

    feladat5

--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp2.Entities;
 
 namespace WindowsFormsApp2
 {
@@ -14,11 +15,25 @@ namespace WindowsFormsApp2
     {
         List<Tick> ticks;
         PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
             ticks = context.Tick.ToList();
             dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            // throw new NotImplementedException();
+            portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = portfolio;
+
         }
     }
 }
